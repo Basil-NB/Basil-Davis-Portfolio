c7b4cddb0d8888e2e577b29a50ad9b4228a4cab7
shane davis <120618274+Basil-NB@users.noreply.github.com>
2023-10-07 21:44:54 -0400

    css footer

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -8,18 +8,18 @@ import hive from '../images/health hive.png';
 const Portfolio = () => {
   return (
     <div className='portfolio'>
-      <div className='card'>
+      <a href='https://nameless-springs-87426-ab7084b2cd1d.herokuapp.com/' className='card'>
         <img src={art} alt="Offres D'art" />
         <p className="description">Description for Offres D'art</p>
-      </div>
-      <div className='card'>
+      </a>
+      <a href='https://molyneauxk93.github.io/PawsPlus-/' className='card'>
         <img src={pets} alt="Paws +" />
         <p className="description">Description for Paws +</p>
-      </div>
-      <div className='card'>
+      </a>
+      <a href='https://www.example.com/link-for-hive' className='card'>
         <img src={hive} alt="Health Hive" />
         <p className="description">Description for Health Hive</p>
-      </div>
+      </a>
     </div>
   );
 };
--- src/components/footer.js
+++ src/components/footer.js
@@ -5,11 +5,14 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-links">
-        <button>GitHub</button>
-        <button>Linkedin</button>
+        <div className="button-container">
+          <button>GitHub</button>
+          <button>LinkedIn</button>
+        </div>
       </div>
     </footer>
   );
 };
+
 
 export default Footer;
